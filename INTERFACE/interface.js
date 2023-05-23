c3b466672074ba51b27a2eda000ec0ba825358f9
Tiago Ferreira da Silva <132114628+TheGamer1932@users.noreply.github.com>
2023-05-23 15:01:35 -0300

    Add files via upload

--- INTERFACE/interface.js
+++ INTERFACE/interface.js
@@ -1,6 +1,30 @@
 const btns = document.querySelectorAll(".btn");
 const btnSair = document.querySelector(".sair");
 
+if ('autoplay' in HTMLAudioElement.prototype) {
+  var backgroundMusic = document.getElementById('backgroundMusic');
+  var currentMusicIndex = localStorage.getItem('currentMusicIndex');
+  var musicList = [
+    // Adicinar as músicas aqui
+     'Pink-Panther-Theme.mp3',
+     'Pink-Panther-Theme.mp3'
+   ];
+  
+  if (currentMusicIndex !== null) {
+    backgroundMusic.src = musicList[currentMusicIndex];
+    backgroundMusic.play().catch(function() {
+      // O navegador bloqueou a reprodução automática; aguardar interação do usuário
+    });
+  }
+
+  // Iniciar a reprodução de áudio quando o usuário interagir com a página
+  document.addEventListener('click', function() {
+    if (backgroundMusic.paused) {
+      backgroundMusic.play();
+    }
+  });
+}
+
 btns.forEach(function (btn) {
    btn.addEventListener("click", function (e) {
       const styles = e.currentTarget.classList;
